08f09429d83ff3a18ef8a0e33af0d38da851dfb6
drsherlock <zain.delhi27@hotmail.com>
2016-07-09 21:21:32 +0530

    First commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name jobSeekerApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the jobSeekerApp
+ */
+angular.module('jobSeekerApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
