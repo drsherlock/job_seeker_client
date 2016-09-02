ac42cea229ec0aac31b8da3f9481c232f3d8fdf2
drsherlock <zain.delhi27@hotmail.com>
2016-09-02 17:10:53 +0530

    Add custom directive for search

---
+++ app/scripts/directives/searchboxdirective.js
@@ -0,0 +1,23 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name jobSeekerApp.directive:searchBox
+ * @description
+ * # searchBox
+ */
+angular.module('jobSeekerApp')
+  .directive('searchBoxDirective', function () {
+    return {
+      restrict: 'E',
+      scope: { 'obj': "=",
+             },
+      controller: function () {},
+      controllerAs: 'dirCtrl',
+      bindToController: true,
+      templateUrl: 'templates/searchbox-template.html',
+      link: function(scope){
+      	console.log(scope);
+      }
+    };
+  });
