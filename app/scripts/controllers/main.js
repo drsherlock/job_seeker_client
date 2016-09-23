2c647d0a453d951a5b9859ebb3b94921b8e7ef28
drsherlock <zain.delhi27@hotmail.com>
2016-09-23 23:44:53 +0530

    Improve design

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,38 +8,44 @@
  * Controller of the jobSeekerApp
  */
 angular.module('jobSeekerApp')
-  .controller('MainCtrl', ['getAllCompanies', 'getAllJobs', function (companiesService, jobsService) {
+	.controller('MainCtrl', ['getAllCompanies', 'getAllJobs', function (companiesService, jobsService) {
 	var ctrl = this;
 	companiesService.getCompanies(1)
-  		.then(function(response){
-        ctrl.companiesCount = response.data.count;
-        $({countNum: $('#companiesCounter').text()}).animate({countNum: ctrl.companiesCount}, {
-          duration: 3000,
-          easing:'linear',
-          step: function() {
-            $('#companiesCounter').text(Math.floor(this.countNum));
-          },
-          complete: function() {
-            $('#companiesCounter').text(this.countNum);
-          }
-        });
-  		}, function(error) {
-  			console.log(error);
-  		});
-  	jobsService.getJobs(1)
-  		.then(function(response){
-        ctrl.jobsCount = response.data.count;
-  			$({countNum: $('#jobsCounter').text()}).animate({countNum: ctrl.jobsCount}, {
-          duration: 3000,
-          easing:'linear',
-          step: function() {
-            $('#jobsCounter').text(Math.floor(this.countNum));
-          },
-          complete: function() {
-            $('#jobsCounter').text(this.countNum);
-          }
-        });
-  		}, function(error) {
-  			console.log(error);
-  		});    
-  }]);
+			.then(function(response){
+				ctrl.companiesCount = response.data.count;
+				$({countNum: $('#companiesCounter').text()}).animate({countNum: ctrl.companiesCount}, {
+					duration: 3000,
+					easing:'linear',
+					step: function() {
+						$('#companiesCounter').text(Math.floor(this.countNum));
+					},
+					complete: function() {
+						$('#companiesCounter').text(this.countNum);
+					}
+				});
+			}, function(error) {
+				console.log(error);
+			});
+		jobsService.getJobs(1)
+			.then(function(response){
+				ctrl.jobsCount = response.data.count;
+				$({countNum: $('#jobsCounter').text()}).animate({countNum: ctrl.jobsCount}, {
+					duration: 3000,
+					easing:'linear',
+					step: function() {
+						$('#jobsCounter').text(Math.floor(this.countNum));
+					},
+					complete: function() {
+						$('#jobsCounter').text(this.countNum);
+					}
+				});
+			}, function(error) {
+				console.log(error);
+			});    
+
+			// Change navbar active class
+			$(".nav a").on("click", function() {
+				$(".nav").find(".active").removeClass("active");
+				$(this).parent().addClass("active");
+			});
+	}]);
